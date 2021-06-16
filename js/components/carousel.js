e7d3d49331e01cad41974954f6dd4dcfe92e3215
Ramiro Garcia <ramirogarciaweb@gmail.com>
2021-06-16 15:35:16 -0300

    carrusel

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,10 +1,12 @@
-new Glider(document.querySelector(".carousel__list"), {
-  slidesToScroll: 1,
-  slidesToShow: 1.3,
-  draggable: true,
-  dots: ".carousel_indicadores",
-  arrows: {
-    prev: ".carousel__anterior",
-    next: ".carousel__siguiente",
-  },
+window.addEventListener("load", () => {
+  new Glider(document.querySelector(".carousel__list"), {
+    slidesToScroll: 1,
+    slidesToShow: 1.2,
+    draggable: true,
+    dots: ".carousel_indicadores",
+    arrows: {
+      prev: ".carousel__anterior",
+      next: ".carousel__siguiente",
+    },
+  });
 });
